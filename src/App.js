8cc0a0852e46b4d03ac944b3ee510332f61e8d40
Anderson <andersonalencarbarros@gmail.com>
2019-11-10 11:20:52 -0300

    componentes atualizados

--- src/App.js
+++ src/App.js
@@ -1,21 +1,113 @@
-import React from 'react';
-import {Bar} from 'react-chartjs-2';
-import GraficoBarra from './components/GraficoBarra'
-import GraficoLinha from './components/GraficoLinha'
-import Chart from './components/Chart'
+import React, { Component } from 'react';
+import logo from './logo512.png';
+import './App.css';
+import Chart from './components/Chart';
 
-class App extends React.Component {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
+
   render() {
     return (
-      // <div>
-      //   <GraficoBarra />
-      // </div> 
-      <div>
-        <GraficoLinha name="Batimentos"/>
-        <GraficoLinha name="Ronco"/>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" width="50px" height= "50px" />
+          <h3 className="mainTitle">How2usleep</h3>
+        </div>
+        <Chart chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom"/>
       </div>
     );
   }
 }
 
-export default App
+export default App;
+
+
+// import React from 'react';
+// import {Bar} from 'react-chartjs-2';
+// import GraficoBarra from './components/GraficoBarra'
+// import GraficoLinha from './components/GraficoLinha'
+// import Chart from './components/Chart'
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       // <div>
+//       //   <GraficoBarra />
+//       // </div> 
+//       <div>
+//         <GraficoLinha name="Batimentos"/>
+//         <GraficoLinha name="Ronco"/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App
+
+
+  // getChartData(){
+  //   // Ajax calls here
+  //   this.setState({
+  //     chartData:{
+  //       labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+  //       datasets:[
+  //         {
+  //           label:'Population',
+  //           data:[
+  //             617594,
+  //             181045,
+  //             153060,
+  //             106519,
+  //             105162,
+  //             95072
+  //           ],
+  //           backgroundColor:[
+  //             'rgba(255, 99, 132, 0.6)',
+  //             'rgba(54, 162, 235, 0.6)',
+  //             'rgba(255, 206, 86, 0.6)',
+  //             'rgba(75, 192, 192, 0.6)',
+  //             'rgba(153, 102, 255, 0.6)',
+  //             'rgba(255, 159, 64, 0.6)',
+  //             'rgba(255, 99, 132, 0.6)'
+  //           ]
+  //         }
+  //       ]
+  //     }
+  //   });
+  // }
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,41 +1,74 @@
-// import React from 'react';
-// import {Bar,Line,Pie} from 'react-chartjs-2';
+import React, {Component} from 'react';
+import {Bar, Line, Pie} from 'react-chartjs-2';
 
-// class Chart extends React.Component{
-//     constructor(props){
-//         super(props)
-//         this.state = {
-//             chartData:{
-//                 labels: ['Boston', 'Santa Maria', 'Juazeiro'],
-//                 datasets: [
-//                     {
-//                         label: 'Population',
-//                         data:[
-//                             1,2,3
-//                         ],
+class Chart extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:props.chartData
+    }
+  }
 
-//                     }
+  // 'defaultProps define uma propriedade padrao
+  // caso nao seja especificado nenhuma quando passado
+  // e será sobreescrito caso algum seja passado
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend: true,
+    legendPosition:'right',
+    location:'City'
+  }
 
-//                 ]
-//             }
-//         }
-//     }
+  render(){
+    return (
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+  this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
 
+        <Line
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
 
-// 	render(){
-//         return(
-//             <div className="chart">
-//                 <Bar
-//                     data={data}
-//                     width={100}
-//                     height={50}
-//                     options={{ maintainAspectRatio: false }}
-//                 />
+        <Pie
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+      </div>
+    )
+  }
+}
 
-//             </div>
-//         );
-//     }
-
-// }
-
-// export default Chart
+export default Chart;
