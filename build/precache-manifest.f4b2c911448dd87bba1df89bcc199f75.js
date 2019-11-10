455add470eefdba158d6fe53a1266a65380e4f9a
Anderson <andersonalencarbarros@gmail.com>
2019-11-10 11:33:10 -0300

    componentes atualizados

---
+++ build/precache-manifest.f4b2c911448dd87bba1df89bcc199f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12c7fee48387d76e2d607c822224b2c",
+    "url": "/How2uSleepRepo/index.html"
+  },
+  {
+    "revision": "6a90d2a03b2862f663f1",
+    "url": "/How2uSleepRepo/static/css/main.9137a49d.chunk.css"
+  },
+  {
+    "revision": "3d9ffebeb8f167a091fc",
+    "url": "/How2uSleepRepo/static/js/2.5e5d9e11.chunk.js"
+  },
+  {
+    "revision": "6a90d2a03b2862f663f1",
+    "url": "/How2uSleepRepo/static/js/main.22056b6f.chunk.js"
+  },
+  {
+    "revision": "7066c8b34bc15f6fc96b",
+    "url": "/How2uSleepRepo/static/js/runtime-main.bf4f548d.js"
+  },
+  {
+    "revision": "df180eb64df1a38a974babb3c55cb5a9",
+    "url": "/How2uSleepRepo/static/media/logo512.df180eb6.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bfe72af1ddbf36a6039b1ef00fcfec40.js"
+  "/How2uSleepRepo/precache-manifest.f4b2c911448dd87bba1df89bcc199f75.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/How2uSleepRepo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
