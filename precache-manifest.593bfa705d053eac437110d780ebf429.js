c697bfee582ec02d8e792626dc3a1c7093da6ba2
Anderson <andersonalencarbarros@gmail.com>
2019-11-26 23:47:45 -0300

    Updates

---
+++ precache-manifest.593bfa705d053eac437110d780ebf429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b0e94767defe5a4e66a7918a781418",
+    "url": "/How2uSleepRepo/index.html"
+  },
+  {
+    "revision": "cea42eeafde8152cd28b",
+    "url": "/How2uSleepRepo/static/css/main.47a7f208.chunk.css"
+  },
+  {
+    "revision": "976aed2cd30e3a6c170a",
+    "url": "/How2uSleepRepo/static/js/2.7474542f.chunk.js"
+  },
+  {
+    "revision": "cea42eeafde8152cd28b",
+    "url": "/How2uSleepRepo/static/js/main.638fb872.chunk.js"
+  },
+  {
+    "revision": "27d5415d74729fd8cf13",
+    "url": "/How2uSleepRepo/static/js/runtime-main.18a9a8cd.js"
+  },
+  {
+    "revision": "df180eb64df1a38a974babb3c55cb5a9",
+    "url": "/How2uSleepRepo/static/media/logo512.df180eb6.png"
+  }
+]);
